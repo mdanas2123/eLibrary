d49e9e586f72880b60bdc51203cfa8109969f654
mdanas2123 <22bca0239@gmail.com>
2024-04-19 19:34:20 +0530

    add confiq

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
 
 const StartServer = () => {
-    const port = process.env.PORT || 3000;
+    const port = config.port || 3000;
 
     app.listen(port, () => {
         console.log(`listening on port ${port}`);
