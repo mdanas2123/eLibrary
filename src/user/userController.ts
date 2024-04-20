027768b91f3285e9d47b5961f8b808d8d08431c2
mdanas2123 <22bca0239@gmail.com>
2024-04-21 00:53:28 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -47,5 +47,40 @@ const createUser = async (
     } catch (error) {
         return next(createHttpError(500, "error while signing jwt token"));
     }
+};
+
+const loginUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        const error = createHttpError(400, "All feild are required")
+        return next(error);
+    }
+    let user;
+    try {
+        user = await userModel.findOne({ email })
+        if (!user) {
+            const error = createHttpError(404, "User not found")
+            return next(error);
+        }
+    } catch (error) {
+        return next(createHttpError(500, "error while getting user "));
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        const error = createHttpError(400, "Invalid password")
+        return next(error);
+    }
+    try {
+        const token = sign({ sub: user._id }, config.jwtSecret as string, { expiresIn: "7d", algorithm: "HS256" });
+
+        res.status(201).json({ accessToken: token });
+    } catch (error) {
+        return next(createHttpError(500, "error while signing jwt token"));
+    }
 }
-export { createUser };
+
+
+export { createUser, loginUser };
