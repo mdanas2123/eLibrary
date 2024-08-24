c27e1639fcad22465fcba47d3581d08bb3ec8b7e
mdanas2123 <22bca0239@gmail.com>
2024-08-24 19:23:10 +0530

    production setup

--- src/books/bookRouter.ts
+++ src/books/bookRouter.ts
@@ -1,33 +1,35 @@
 import express from "express";
-// import multer from "multer";
+import multer from "multer";
 // import path from "node:path";
 import { createBook, updateBook, bookList, getSinglebook, deleteBook } from "./bookController";
 import authenticate from "../Middleware/authenticate";
 
 const bookRouter = express.Router();
 
-// const upload = multer({
-//     dest: path.resolve(__dirname, "../../public/data/uploads"),
-//     limits: {
-//         fileSize: 1024 * 1024 * 10
-//     }
-// });
+const storage = multer.memoryStorage();
+const upload = multer({
+    // dest: path.resolve(__dirname, "../../public/data/uploads"),
+    storage,
+    limits: {
+        fileSize: 1024 * 1024 * 10
+    }
+});
 
 bookRouter.post("/",
     authenticate,
-    // upload.fields([
-    //     { name: "coverImage", maxCount: 1 },
-    //     { name: "file", maxCount: 1 },
+    upload.fields([
+        { name: "coverImage", maxCount: 1 },
+        { name: "file", maxCount: 1 },
 
-    // ]),
+    ]),
     createBook
 );
 bookRouter.patch("/:bookId",
     authenticate,
-    // upload.fields([
-    //     { name: "coverImage", maxCount: 1 },
-    //     { name: "file", maxCount: 1 }
-    // ]),
+    upload.fields([
+        { name: "coverImage", maxCount: 1 },
+        { name: "file", maxCount: 1 }
+    ]),
     updateBook
 );
 bookRouter.get("/", bookList)
